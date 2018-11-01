dd792306b1f538afa87039892e4380ebaaca8d53
Adam Zieliński <adam@adamziel.com>
2018-11-01 17:22:19 +0100

    Turn store-nux signup flow into ecommerce signup flow

--- client/signup/steps/domains/index.jsx
+++ client/signup/steps/domains/index.jsx
@@ -361,6 +361,7 @@ class DomainsStep extends React.Component {
 				suggestion={ initialQuery }
 				designType={ this.getDesignType() }
 				vendor={ abtest( 'krackenRebootM33' ) }
+				deemphasiseTlds={ this.props.flowName === 'ecommerce' ? [ 'blog' ] : [] }
 			/>
 		);
 	};
